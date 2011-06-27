749b27c86c6c2d8c5e5af0964ce5f130ce2c7fa7
Mark Drzycimski <mark@clickrain.com>
2011-06-27 15:50:22 -0500

    Fixed the geolocation results parsing

--- themes/third_party/cr_locality/php/cr_locality.php
+++ themes/third_party/cr_locality/php/cr_locality.php
@@ -7,60 +7,15 @@ $x = simplexml_load_file($g);
 
 foreach ($x->result->address_component as $c)
 {
-	switch ($c->type)
-	{
-		case 'street_address':
-			$d['precise_address'] = $c->long_name;
-		break;
-		case 'natural_feature':
-			$d['natural_feature'] = $c->long_name;
-		break;
-		case 'airport':
-			$d['airport'] = $c->long_name;
-		break;
-		case 'park':
-			$d['park'] = $c->long_name;
-		break;
-		case 'point_of_interest':
-			$d['point_of_interest'] = $c->long_name;
-		break;
-		case 'premise':
-			$d['named_location'] = $c->long_name;
-		break;
-		case 'street_number':
-			$d['house_number'] = $c->long_name;
-		break;
-		case 'route':
-			$d['street'] = $c->long_name;
-		break;
-		case 'locality':
-			$d['town_city'] = $c->long_name;
-		break;
-		case 'administrative_area_level_3':
-			$d['district_region'] = $c->long_name;
-		break;
-		case 'neighborhood':
-			$d['neighborhood'] = $c->long_name;
-		break;
-		case 'colloquial_area':
-			$d['locally_known_as'] = $c->long_name;
-		break;
-		case 'administrative_area_level_2':
-			$d['county_state'] = $c->long_name;
-		break;
-		case 'postal_code':
-			$d['postcode'] = $c->long_name;
-		break;
-		case 'country':
-			$d['country'] = $c->long_name;
-		break;
-	}
+	$c = (array) $c;
+	$k = (is_array($c['type']))? $c['type'][0]: $c['type'];
+	$d[$k] = $c['long_name'];
 }
 
 list($lat,$lon) = explode(',',$ll);
 $d['latitude'] = $lat;
 $d['longitude'] = $lon;
-$d['formatted_address'] = $x->result->formatted_address;
+$d['formatted_address'] = (string) $x->result->formatted_address;
 $d['accuracy'] = htmlentities(htmlspecialchars(strip_tags($_GET['accuracy'])));
 $d['altitude'] = htmlentities(htmlspecialchars(strip_tags($_GET['altitude'])));
 $d['altitude_accuracy'] = htmlentities(htmlspecialchars(strip_tags($_GET['altitude_accuracy'])));
